209d70590620989a84d929f88e95e1aaced705b2
maylukk <142520679+maylukk@users.noreply.github.com>
2025-05-28 20:59:28 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,23 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = documemt.querySelectorAll(".aba-conteudo");
-for (let i=0; i < botoes.length; i++){
 
-
-    botoes[i].onlick=function{
-
-        for(let j=0; j< botoes.length; j++){
-            botoes[j]. classList.remove("ativo")
-            textos[j].classlist.remove("ativo");
-        }
-    }
-       botoes[i]
+for (let i=0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+        for(let j = 0; j < botoes.length; j++){
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
 }
+
+        botoes[i].classList.add("ativo");
+        textos{i}.classList.add("ativo");
+    }
+}
+
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
